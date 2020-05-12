1951e3a465a75e4f07cc83a93905989cd488d6ba
tahirata83 <tahiratayev@ichc.ro>
2020-05-12 07:57:18 +0300

    first

---
+++ src/Laptop/Calculation.java
@@ -0,0 +1,64 @@
+package Laptop;
+
+import java.util.ArrayList;
+
+public class Calculation {
+
+     /*
+        Create method name is getRam
+
+        Parameter is ArrayList<String>
+
+        if ram is 32 gb add 300 to price
+        if ram is 16 gb add 200 to price
+        if ram is 8 gb add 100 to price
+        if ram is 4 gb add 50 to price
+
+        return the price.
+     */
+
+
+    /*
+       Create method name is getCPU
+
+        Parameter is ArrayList<String>
+
+        if CPU is i3 add 200 to price
+        if CPU is i5 add 300 to price
+        if CPU is i7 add 500 to price
+
+        return the price.
+     */
+
+
+    /*
+      Create method name is getColor
+
+      Parameter is ArrayList<String>
+
+      return type is int
+
+      if Color is Red add 400 to price
+      if Color is Orange add 300 to price
+      if Color is Silver add 200 to price
+      if Color is Black add 150 to price
+
+      return the price.
+   */
+
+    /*
+      Create method name is getSize
+
+      return type is int
+
+      Parameter is ArrayList<String>
+
+      if Size contains "Mini" add 100 to price
+      if Size contains "Middle" add 200 to price
+      if Size contains "Max" add 300 to price
+
+      return the price.
+   */
+
+
+}
---
+++ src/Laptop/PriceForOneLaptop.java
@@ -0,0 +1,53 @@
+package Laptop;
+
+import java.util.ArrayList;
+
+public class PriceForOneLaptop {
+
+    /*
+        Given one method name is getPrice
+
+        Parameter is int
+
+        Return type is int
+
+        This method is calling the AllData method from Data class. Laptop ID is a parameter laptopID
+
+        This method need to call all other methods in Calculation class and get the price and return that total Amount.
+     */
+
+    public int getPrice(int laptopID){
+
+
+        return 0;
+    }
+
+    /*
+        Given getDiscount
+
+        Parameter is two int (amount , discount)
+
+        Return type is int
+
+        Make the discount from the amount
+
+        And Return the discounted price
+
+        Example
+
+        Amount 100
+        discount 20
+
+        After a 20% discount
+
+        Result should be 80
+     */
+
+    public int getDiscount(int num , int discount){
+
+        return 0;
+    }
+
+
+
+}
